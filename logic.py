e128b93ea9ac95eb21cb98d2eeeeaeea7632ef7a
hanhanpradja <hanafipradja01@gmail.com>
2026-02-17 21:58:39 +0700

    commit pertama dari kodland

--- logic.py
+++ logic.py
@@ -32,3 +32,12 @@ class Pokemon:
 
     async def show_img(self):
         # Metode asinkron untuk mengambil URL gambar pokémon melalui PokeAPI
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    img_url = data['sprites']['front_default']
+                    return img_url
+                else:
+                    return None
